336daeba746aedd545198e49144f6f0e1c3abfa0
BlackPearlescence <srogers138@gmail.com>
2025-01-13 14:04:01 -0500

    fix navbar styling

--- personalwebsite/app/components/NavBar.tsx
+++ personalwebsite/app/components/NavBar.tsx
@@ -1,13 +1,26 @@
 import styles from "./NavBar.module.scss";
 import Link from "next/link";
+import { FaHome, FaInfo, FaProjectDiagram, FaBlog} from "react-icons/fa";
 
 export const NavBar = () => {
     return (
         <nav className={styles.navbar}>
-            <Link href="/">Home</Link>
-            <a href="#">About Me</a>
-            <a href="#">Projects</a>
-            <a href="#">Blog</a>
+            <div>
+                <Link href="/">Home </Link>
+                <FaHome/>
+            </div>
+            <div>
+                <Link href="/aboutme">About Me </Link>
+                <FaInfo/>
+            </div>
+            <div>
+                <Link href="/projects">Projects </Link>
+                <FaProjectDiagram/>
+            </div>
+            <div>
+                <Link href="/blog">Blog </Link>
+                <FaBlog/>
+            </div>
         </nav>
     )
 }
